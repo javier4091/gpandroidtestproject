9a21ebb5d14b373823df785357ec627e6948091c
jose <javier4091@yahoo.com>
2019-05-10 16:27:18 -0400

    added library that will be used to call api, added client that uses the library

--- app/src/main/java/com/demo/gpusersapp/MainActivity.java
+++ app/src/main/java/com/demo/gpusersapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.demo.gpusersapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.loopj.android.http.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test
+
+
+
+
     }
 }
