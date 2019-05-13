2c886712e342f7ee4865a5b65a076fe74cafa294
jose <javier4091@yahoo.com>
2019-05-12 20:44:19 -0400

    finished implementation of registeration dialog

--- app/src/main/java/com/demo/gpusersapp/MainActivity.java
+++ app/src/main/java/com/demo/gpusersapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.demo.gpusersapp;
 
+import android.app.*;
+import android.content.DialogInterface;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Toast;
 
@@ -22,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     //Show dialog to add new user when button is pressed
     public void showAddUserDialog(View view)
     {
-        
+        DialogFragment newFragment = new RegisterDialogFragment();
+        newFragment.show(getSupportFragmentManager(), "register");
     }
+
 }
--- app/src/main/java/com/demo/gpusersapp/RegisterDialogFragment.java
+++ app/src/main/java/com/demo/gpusersapp/RegisterDialogFragment.java
@@ -1,4 +1,80 @@
 package com.demo.gpusersapp;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegisterDialogFragment {
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+
+public class RegisterDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.dialog_register, null))
+                // Add action buttons
+                .setPositiveButton("Register", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(final DialogInterface dialog, int id) {
+
+                        Dialog d = (Dialog)dialog;
+                        EditText emailTxt = (EditText)d.findViewById(R.id.email);
+                        EditText passTxt = (EditText)d.findViewById(R.id.password);
+
+                        RequestParams params = new RequestParams();
+                        params.put("email", emailTxt.getText().toString());
+                        params.put("password", emailTxt.getText().toString());
+                        reqresApiClient.post("register", params, new JsonHttpResponseHandler(){
+                            @Override
+                            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                                //System.out.println("");
+                                    Toast toast = Toast.makeText(((Dialog) dialog).getContext(),"Registration Successful", Toast.LENGTH_SHORT);
+                                    toast.show();
+                                    
+                            }
+
+                            @Override
+                            public void onFailure(int statusCode, Header[] headers , Throwable e , JSONObject response) {
+                                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                                //System.out.println("");
+                                try
+                                {
+                                    Toast toast = Toast.makeText(((Dialog) dialog).getContext(), response.getString("error"), Toast.LENGTH_SHORT);
+                                    toast.show();
+
+
+                                }
+                                catch (JSONException ex)
+                                {
+                                    //unable to parse response
+                                }
+                            }
+                        });
+
+                    };
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        RegisterDialogFragment.this.getDialog().cancel();
+                    }
+                });
+        return builder.create();
+    }
 }
